d2ccccdbc535a0938aa5398e4fe9c7a1e9f4828c
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2020-07-13 12:35:55 +0300

    Some changes in class structure

--- DustFractals/DustFractals/CompressReflactionFractal.cs
+++ DustFractals/DustFractals/CompressReflactionFractal.cs
@@ -8,16 +8,11 @@ namespace DustFractals
 {
     class CompressReflactionFractal : AbstarctDustFractal
     {
-        float _a, _b;
-
         public CompressReflactionFractal(float a, float b)
         {
-            _a = a;
-            _b = b;
-
-            _L = new Matrix { X11 = _a, X12 = _b, X21 = _b, X22 = -_a };
-            _R = new Matrix { X11 = _a, X12 = -_b, X21 = -_b, X22 = -_a };
-            _Vr = new Vector { X = _a, Y = _b };
+            _L = new Matrix { X11 = a, X12 = b, X21 = b, X22 = -a };
+            _R = new Matrix { X11 = a, X12 = -b, X21 = -b, X22 = -a };
+            _Vr = new Vector { X = a, Y = b };
         }
     }
 }
---
+++ DustFractals/DustFractals/RotationCompressFractal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DustFractals
+{
+    class RotationCompressFractal : AbstarctDustFractal
+    {
+        public RotationCompressFractal(float a, float b, float c, float d)
+        {
+            _L = new Matrix { X11 = a, X12 = -b, X21 = b, X22 = a };
+            _R = new Matrix { X11 = c, X12 = -d, X21 = d, X22 = c };
+            _Vr = new Vector { X = 1 - c, Y = -d };
+        }
+    }
+}
